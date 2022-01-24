f07ca240cf2b6c8d5d88221ed33288c32d0c7d88
pasthortown <luissalazarvaca1986@gmail.com>
2022-01-24 08:29:16 -0500

    calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sitio';
+  numero1 = 0;
+  numero2 = 0;
+  resultado = 0;
+
+  calcular(operacion: string) {
+    this.resultado = 0;
+    switch (operacion) {
+      case 'potencia':
+        this.resultado = 1;
+        for(let i = 0; i< this.numero2; i++) {
+          this.resultado = this.resultado * this.numero1;
+        }
+        break;
+      case 'suma':
+        // Magaly
+        break;
+      case 'resta':
+        // Josselyn
+        break;
+      case 'multiplicacion':
+        // Jhon
+        break;
+      case 'division':
+        // Tyron
+        break;
+    }
+  }
 }
